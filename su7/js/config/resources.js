f7f7764e2ecc857628d075de08fcf309042d0cd0
Trevor <hanmingbin@sinosoft.com.cn>
2024-07-08 02:46:32 +0800

    优化目录结构

--- su7/js/config/resources.js
+++ su7/js/config/resources.js
@@ -7,11 +7,11 @@ const LoaderType ={
 
 export default [
     // { name: 'sceneLoad',  type: LoaderType.RGBE, path: '../img/sky.hdr' },
-    { name: 'roadMesh',  type: LoaderType.Texture, path: '../img/banmaxian.jpg' },
-    { name: 'otherCarLoad',  type: LoaderType.GLTF, path: '../models/gltf/otherCar.glb' },
-    // { name: 'carLoad',  type: LoaderType.GLTF, path: '../models/gltf/su7-car.glb' },
-    { name: 'carLoad',  type: LoaderType.GLTF, path: '../models/gltf/su7-car_edit.glb' },
-    { name: 'carCurveLoad',  type: LoaderType.GLTF, path: '../models/gltf/xiedu.glb' },
-    { name: 'cubeGround',  type: LoaderType.GLTF, path: '../models/gltf/cubeGround.glb' },
-    { name: 'tunnelLoader',  type: LoaderType.GLTF, path: '../models/gltf/tunnel.glb' }
+    { name: 'roadMesh',  type: LoaderType.Texture, path: './img/banmaxian.jpg' },
+    { name: 'otherCarLoad',  type: LoaderType.GLTF, path: './models/gltf/otherCar.glb' },
+    // { name: 'carLoad',  type: LoaderType.GLTF, path: './models/gltf/su7-car.glb' },
+    { name: 'carLoad',  type: LoaderType.GLTF, path: './models/gltf/su7-car_edit.glb' },
+    { name: 'carCurveLoad',  type: LoaderType.GLTF, path: './models/gltf/xiedu.glb' },
+    { name: 'cubeGround',  type: LoaderType.GLTF, path: './models/gltf/cubeGround.glb' },
+    { name: 'tunnelLoader',  type: LoaderType.GLTF, path: './models/gltf/tunnel.glb' }
 ]
