ede1a3578638043a320cec0fc02480396b1eaee0
Trevor <hanmingbin@sinosoft.com.cn>
2024-07-11 08:43:36 +0800

    添加shader场景控制、shader流光控制

---
+++ su7/js/shaders/DynamicEnv/vert.js
@@ -0,0 +1,16 @@
+
+export const vertexShader = `
+        uniform float iTime;
+        uniform vec3 iResolution;
+        uniform vec4 iMouse;
+        
+        varying vec2 vUv;
+        
+        void main(){
+            vec3 p=position;
+            gl_Position=vec4(p,1.);
+            
+            vUv=uv;
+        }
+
+`
