54ca07fcb6c3ebc370ed2af0f4fc01d4224be81f
Trevor <hanmingbin@sinosoft.com.cn>
2024-08-15 06:05:25 +0800

    分离项目

--- su7/js/shaders/wind/vertex_line.js
+++
@@ -1,9 +0,0 @@
-export const vertexShader = `
-varying vec2 vUv;
-void main() {
-  vUv = uv;
-  vec4 mvPosition = modelViewMatrix * vec4( position, 1. );
-  // gl_PointSize = 10. * ( 1. / - mvPosition.z );
-  gl_Position = projectionMatrix * mvPosition;
-}
-`
